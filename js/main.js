8e71a83b9d0138ea3c462ed90ce93b31b759c078
capurrex <capurrex@gmail.com>
2023-02-05 13:56:10 -0300

    agregado de arrays decolores y aromas, captura de nodos catalogo y match

--- js/main.js
+++ js/main.js
@@ -5,27 +5,33 @@ class velas {
     this.precio = precio;
     this.imagen = imagen;
     this.imgAlt = imgAlt;
+    this.descripcion=descripcion;
   }
 }
 
 //funciones
 
 
+
+//Capturas nodos DOM:
+let catalogo = document.getElementById("catalogo")
+let match = document.getElementById("match")
+
 //instanciacion
 
-const vela1 = new velas("Woolen ball", 150, "imgs/woolen.jpg","imagen vela woolen ball");
-const vela2 = new velas("Bubble", 350,"imgs/bubble.jpg", "imagen vela bubble" );
-const vela3 = new velas("Mini bubble", 100, "imgs/minibubble.jpg", "imagen vela mini bubble");
-const vela4 = new velas("Onion", 180,"imgs/onion.jpg", "imagen vela onion");
-const vela5 = new velas("Luna llena", 550,"imgs/lunallena.jpg", "imagen vela luna llena");
-const vela6 = new velas("Luna nueva", 450,"imgs/lunanueva.jpg", "imagen vela luna nueva");
-const vela7 = new velas("Cuarto creciente", 350,"imgs/cuartocreciente.jpg", "imagen vela cuarto creciente" );
-const vela8 = new velas("Cuarto menguante", 300 ,"imgs/cuartomenguante.jpg", "imagen vela cuarto menguante");
-const vela9 = new velas("Yue", 200,"imgs/yue.jpg", "imagen vela yue");
-const vela10 = new velas("Lúa", 200,"imgs/lua.jpg", "imagen vela lua");
-const vela11 = new velas( "Bombones de cera X 5", 100,"imgs/bombones.jpg", "imagen de los bombones de aroma" );
-const vela12 = new velas( "Hornito + 5 bombones y 2 velas de encendido", 350,"imgs/hornito.jpg", "imagen hornito y bombones");
-const vela13 = new velas( "Flower", 150,"imgs/.jpg", "imagen vela");
+const vela1 = new velas("Woolen ball", 150, "imgs/woolen.jpg","imagen vela woolen ball", "pedir la descripcion");
+const vela2 = new velas("Bubble", 350,"imgs/bubble.jpg", "imagen vela bubble", "pedir la descripcion" );
+const vela3 = new velas("Mini bubble", 100, "imgs/minibubble.jpg", "imagen vela mini bubble", "pedir la descripcion");
+const vela4 = new velas("Onion", 180,"imgs/onion.jpg", "imagen vela onion", "pedir la descripcion");
+const vela5 = new velas("Luna llena", 550,"imgs/lunallena.jpg", "imagen vela luna llena", "pedir la descripcion");
+const vela6 = new velas("Luna nueva", 450,"imgs/lunanueva.jpg", "imagen vela luna nueva", "pedir la descripcion");
+const vela7 = new velas("Cuarto creciente", 350,"imgs/cuartocreciente.jpg", "imagen vela cuarto creciente", "pedir la descripcion" );
+const vela8 = new velas("Cuarto menguante", 300 ,"imgs/cuartomenguante.jpg", "imagen vela cuarto menguante", "pedir la descripcion");
+const vela9 = new velas("Yue", 200,"imgs/yue.jpg", "imagen vela yue", "pedir la descripcion");
+const vela10 = new velas("Lúa", 200,"imgs/lua.jpg", "imagen vela lua", "pedir la descripcion");
+const vela11 = new velas( "Bombones de cera X 5", 100,"imgs/bombones.jpg", "imagen de los bombones de aroma", "pedir la descripcion" );
+const vela12 = new velas( "Hornito + 5 bombones y 2 velas de encendido", 350,"imgs/hornito.jpg", "imagen hornito y bombones", "pedir la descripcion");
+const vela13 = new velas( "Flower", 150,"imgs/.jpg", "imagen vela", "pedir la descripcion");
 
 
 const grupoVelas = [
@@ -44,7 +50,7 @@ const grupoVelas = [
   vela13,
 ];
 
-const aromas = [
+const aromasVelas = [
   "Limon",
   "Lavanda",
   "Mango",
@@ -55,6 +61,9 @@ const aromas = [
   "Canela",
 ];
 
+const coloresVelas= [
+  "rosa", "verde", "azul", "naranja", "amarillo", "gris", "beige", "blanco", "violeta"
+]
 // captura de nodos
 
 
